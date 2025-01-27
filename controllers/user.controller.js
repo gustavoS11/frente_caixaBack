0d625d49b22194b47cf28ea01c141ec1ad8d24ee
Gustavo <gustavohenrique6224@gmail.com>
2025-01-27 19:57:56 -0300

    removido dois console.log

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,9 +1,7 @@
 import { getUsuarioModel} from "../models/user.model.js"
 
 export async function login(req, res) {
-    console.log("chegou no controller")
     const dados = req.body
-    console.log(dados)
     const user = await getUsuarioModel(dados)
     if (user.length == 0) {
         return res.status(404).json({ erro: 'email inválido' })
